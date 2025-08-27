2d21ea13d7ff27e2fb39de34bc6fb20413f0c655
NickSarychev <inobeatt@gmail.com>
2025-08-27 15:04:33 +0300

    handler and gin

---
+++ pkg/handler/hendler.go
@@ -0,0 +1,37 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) initRouter() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up")
+		auth.POST("/sign-in")
+	}
+	api := router.Group("/api")
+	{
+		lists := api.Group("/api")
+		{
+			lists.POST("/")
+			lists.GET("/")
+			lists.GET("/:id")
+			lists.PUT("/:id")
+			lists.DELETE("/:id")
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/")
+				items.GET("/")
+				items.GET("/:item_id")
+				items.PUT("/:id")
+				items.DELETE("/:id")
+			}
+		}
+	}
+	return router
+}
