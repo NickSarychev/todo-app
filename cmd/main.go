0c16b91535359b82b5440401cd9b6eeff4a19f05
NickSarychev <inobeatt@gmail.com>
2025-08-28 13:30:46 +0300

    add db

--- cmd/main.go
+++ cmd/main.go
@@ -15,18 +15,35 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("failed to inicialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config: %v", err)
+	}
+
 }
 
 func initConfig() error {
 	viper.AddConfigPath("configs")
-	viper.SetConfigFile("config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
 	return viper.ReadInConfig()
-
 }
