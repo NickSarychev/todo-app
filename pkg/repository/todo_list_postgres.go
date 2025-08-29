69a901eaabbdbde525f2bb877c1a12dc5c0a05ef
NickSarychev <inobeatt@gmail.com>
2025-08-29 23:19:09 +0300

    corrected sql query

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -84,7 +84,7 @@ func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput
 	*/
 	setQuery := strings.Join(setValues, ", ")
 
-	query := fmt.Sprintf("UPDATE %s tl SET %s FROM %s ul WHERE tl.id = ul.list_id AND ul.list_id=%d AND ul.user_ud=$%d",
+	query := fmt.Sprintf("UPDATE %s tl SET %s FROM %s ul WHERE tl.id = ul.list_id AND ul.list_id=$%d AND ul.user_id=$%d",
 		todoListsTable, setQuery, usersListsTable, argId, argId+1)
 
 	args = append(args, listId, userId)
