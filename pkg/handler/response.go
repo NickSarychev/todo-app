8c53e82442a9a6e2223057963b067a52c5b21a29
NickSarychev <inobeatt@gmail.com>
2025-08-29 22:53:36 +0300

    add update

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -8,6 +8,9 @@ import (
 type errorResponce struct {
 	Massage string `json:"massage"`
 }
+type statusResponce struct {
+	Status string `json:"status"`
+}
 
 func newErrorResponse(c *gin.Context, statusCode int, massage string) {
 	logrus.Error(massage)
