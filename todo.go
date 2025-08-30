4be18b980f2329441ec07e43601db9a9a36aa733
NickSarychev <inobeatt@gmail.com>
2025-08-30 10:57:20 +0300

    CRUD

--- todo.go
+++ todo.go
@@ -15,16 +15,16 @@ type UsersList struct {
 }
 
 type TodoItem struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
+	Done        bool   `json:"done" db:"done"`
 }
 
 type ListsItem struct {
-	Id     int
-	ListId int
-	ItemId int
+	Id     int `json:"id" db:"id"`
+	ListId int `json:"list_id"`
+	ItemId int `json:"item_id"`
 }
 
 type UpdateListInput struct {
