3c893bed9af09bca6c1523b71f24431285e7f6d3
NickSarychev <inobeatt@gmail.com>
2025-08-31 13:59:34 +0300

    main gorutine

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/NickSarychev/todo-app"
 	"github.com/NickSarychev/todo-app/pkg/handler"
@@ -46,12 +49,25 @@ func main() {
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running http server %s", err.Error())
+		}
+		if err := viper.ReadInConfig(); err != nil {
+			logrus.Fatalf("Error reading config: %v", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
 
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("error occured while running http server %s", err.Error())
+	logrus.Print("TodoApp Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
 	}
-	if err := viper.ReadInConfig(); err != nil {
-		logrus.Fatalf("Error reading config: %v", err)
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured db connection close: %s", err.Error())
 	}
 
 }
